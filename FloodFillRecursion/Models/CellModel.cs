464d039ad4ba8c20c3febd40ada8872b42bc46e1
Keon Bushman <138335528+IPFizzy@users.noreply.github.com>
2026-08-17 16:08:21 -0700

    Clean up cell model

--- FloodFillRecursion/Models/CellModel.cs
+++ FloodFillRecursion/Models/CellModel.cs
@@ -1,27 +1,11 @@
-﻿/*
- * Keon Bushman
- * CST - 250
- * 06/07/2026
- * Flood Fill Recursion
- * Activity 3
- * Activity 3 Guide
- */
-
 namespace FloodFillRecursion.Models
 {
     internal class CellModel
     {
-        // Cell Model Properties
-        public int Row { get; set; }
-        public int Column { get; set; }
+        public int Row { get; }
+        public int Column { get; }
         public string Contents { get; set; }
 
-        /// <summary>
-        /// Parameterized constructor for CellModel
-        /// </summary>
-        /// <param name="row"></param>
-        /// <param name="column"></param>
-        /// <param name="contents"></param>
         public CellModel(int row, int column, string contents)
         {
             Row = row;
